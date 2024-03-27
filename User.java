950943754bbcec2d326c69c13371699b92ad07e8
LuisAbril <146706969+LuisAbril@users.noreply.github.com>
2024-03-27 10:05:58 +0100

    Delete

--- User.java
+++
@@ -1,9 +0,0 @@
-package src.users;
-
-public abstract class User {
-    public User(String name, String nick, String password){
-        this.name = name;
-        this.nick = nick;
-        this.password = password;
-    }
-}
